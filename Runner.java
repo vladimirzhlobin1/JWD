f4fea596d2098a79821656c089f981ca20e23510
test <pravo@oka.by>
2021-11-20 16:13:57 +0300

    First commit add Classes 2-2

--- Runner.java
+++ Runner.java
@@ -1,6 +1,6 @@
 package by.gsu.epamlab;
 
-public classс   Runner {
+public classс  Runner {
 
     public static void main(String[] args) {
 
--- src/by.gsu.epamlab/Material.java
+++ src/by.gsu.epamlab/Material.java
@@ -1,33 +1,32 @@
 package by.gsu.epamlab;
 
 public class Material {
-    private final String nameOfMaterial;
-    private final double densityOfMaterial;
+    private final String name;
+    private final double density;
 
 
     public Material() {
-        super( );
-        this.nameOfMaterial = "";
-        this.densityOfMaterial = 0;
+        this.name = null;
+        this.density = 0;
     }
 
     public Material(String name, double density) {
-        this.nameOfMaterial = name;
-        this.densityOfMaterial = density;
+        this.name = name;
+        this.density = density;
     }
 
     public String getName() {
-        return nameOfMaterial;
+        return name;
     }
 
     public double getDensity() {
-        return densityOfMaterial;
+        return density;
     }
 
     @Override
     public String toString() {
 
-        return "Name of material" + nameOfMaterial + ";" + "DensityOfMaterial" + densityOfMaterial;
+        return nameO + ";" + density;
     }
 
 }
