36c155c0b26fd93d861c2b3fcbe4cc523f9538b2
test <pravo@oka.by>
2021-11-29 23:32:59 +0300

    Commit 3-1-7

--- src/by.gsu.epamlab/RunnerTest.java
+++ src/by.gsu.epamlab/RunnerTest.java
@@ -1,15 +1,13 @@
 package by.gsu.epamlab;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 public class RunnerTest {
     @Test
     public void testGetCost() {
         Purchase p1 = new Purchase(WeekDay.SUNDAY, 2, 5.00);
-        Assert.assertNotSame(7600, p1.getCost());
-        Purchase p2 = new Purchase(WeekDay.MONDAY, 2, 4.00);
-        Assert.assertNotSame(4254, p2.getCost());
+        Assert.assertEquals(1500, p1.getCost());
+        Purchase p2 = new Purchase(WeekDay.MONDAY, 4, 1.00);
+        Assert.assertEquals(3000, p2.getCost());
     }
 
     @Test
