7260ae9d3a1e43d097259143119bf9bd28e6100a
vladimirzhlobin1 <94230029+vladimirzhlobin1@users.noreply.github.com>
2022-02-09 22:00:24 +0300

    String_add_rew

---
+++ src/testRunner1.java
@@ -0,0 +1,106 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Locale;
+import java.util.Scanner;
+import org.junit.Assert;
+import org.junit.Test;
+public class testRunner1 {
+  private static final String MINUS = " - ";
+  private static final String RESULT_HEAD = "result(";
+  private static final String RESULT_TAIL = ") = ";
+  private static final String SEMICOLON = ";";
+  private static final String PLUS = " + ";
+  private static int getResult(String csvName, StringBuilder strResult)
+        throws FileNotFoundException {
+	  int errorLines = 0;
+        try {
+            Scanner sc = new Scanner(new FileReader(csvName));
+            sc.useLocale(Locale.ENGLISH);
+            StringBuilder finalString = new StringBuilder();
+            double result = 0.0;
+            while (sc.hasNextLine()) {
+            	try {
+                String line = sc.nextLine();
+                String[] elements = line.split(SEMICOLON);
+                int index = Integer.parseInt(elements[0]);
+                double number = Double.parseDouble(elements[index]);
+                result += number;
+                finalString.append(number >= 0 ? MINUS : PLUS).append(Math.abs(number));
+                } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                    errorLines++;
+                }
+            	}
+                if (finalString.length() > 0) {
+                final int SINGL = MINUS.length();
+                final char CHAR_MIN = '-';
+                String sym = finalString.substring(0,SINGL);
+                finalString.delete(0, SINGL);
+                if (sym.equals(MINUS)) {
+                	finalString.insert(0, CHAR_MIN);
+                }
+                }
+            finalString.insert(0,RESULT_HEAD).append(RESULT_TAIL).append(result);
+        }finally {
+		}  
+        return errorLines;
+    }
+ @Test
+  public void testMainScenario() throws FileNotFoundException {
+    String csv1 = "src/in1.csv";
+    String res = String.format("5.2%s3.14%s0.0",MINUS,PLUS);
+    String sum = "2.06";
+      StringBuilder res2 = new StringBuilder();
+      int errorLines = getResult(csv1, res2);
+      Assert.assertEquals(3, errorLines);
+      String expectedIn1 = String.format("%s%s%s%s",RESULT_HEAD, res, RESULT_TAIL, sum);
+      Assert.assertEquals(expectedIn1, res2.toString());
+  }
+  @Test
+  public void testMainScenario2() throws FileNotFoundException {
+    String csv2 = "src/in2.csv";
+    String res = String.format("-3.1%s1.0",MINUS);
+    String sum = "-4.1";
+      StringBuilder res2 = new StringBuilder();
+      int errorLines = getResult(csv2, res2);
+      Assert.assertEquals(0, errorLines);
+      String expectedIn1 = String.format("%s%s%s%s",RESULT_HEAD, res, RESULT_TAIL, sum);
+      Assert.assertEquals(expectedIn1, res2.toString());
+  }
+  @Test
+  public void testMainScenario3() throws FileNotFoundException {
+    String csv2 = "src/in3.csv";
+    String res = "0.75";
+    String sum = "0.75";
+      StringBuilder res2 = new StringBuilder();
+      int errorLines = getResult(csv2, res2);
+      Assert.assertEquals(0, errorLines);
+      String expectedIn1 = String.format("%s%s%s%s",RESULT_HEAD, res, RESULT_TAIL, sum);
+      Assert.assertEquals(expectedIn1, res2.toString());}
+  @Test
+  public void testMainScenario4() throws FileNotFoundException {
+    String csv2 = "src/in4.csv";
+    String res = "0.0";
+    String sum = "0.0";
+      StringBuilder res2 = new StringBuilder();
+      int errorLines = getResult(csv2, res2);
+      Assert.assertEquals(0, errorLines);
+      String expectedIn1 = String.format("%s%s%s%s",RESULT_HEAD, res, RESULT_TAIL, sum);
+      Assert.assertEquals(expectedIn1, res2.toString());}
+  
+  @Test
+  public void testMainScenario5() throws FileNotFoundException {
+    String csv2 = "src/in5.csv";
+    String res="";
+    String sum = "0.0";
+      StringBuilder res2 = new StringBuilder();
+      int errorLines = getResult(csv2, res2);
+      Assert.assertEquals(1, errorLines);
+      String expectedIn1 = String.format("%s%s%s%s",RESULT_HEAD, res, RESULT_TAIL, sum);
+      Assert.assertEquals(expectedIn1, res2.toString());}
+  
+  
+  @Test(expected = FileNotFoundException.class)
+  public void testFileNotFoundEx() throws FileNotFoundException {
+      getResult("wrongName", new StringBuilder());
+  }    
+}
