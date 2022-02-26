e15ef177195b6ed653904b315fce873ca5c6cd63
vladimirzhlobin1 <94230029+vladimirzhlobin1@users.noreply.github.com>
2022-02-26 20:18:06 +0300

    add converted files af.rew.Strings2

--- testRunner.java
+++ testRunner.java
@@ -4,14 +4,17 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.junit.Assert;
 import org.junit.Test;
+
 public class TestRunner {
     final static String KEY_FIRST = "index(.*)";
     final static String NUM_SEARCH = "[1-9]\\d*";
     final static String VALUE = "value";
     static Pattern pattern = Pattern.compile(KEY_FIRST);
     static Pattern pattern2 = Pattern.compile(NUM_SEARCH);
+
     private static int getResult(String g) throws FileNotFoundException {
         double sum = 0.0;
         int errors = 0;
@@ -42,44 +45,61 @@ public class TestRunner {
         } catch (MissingResourceException e) {
             System.out.println("No file...");
         }
-        return new Result(sum, errors);;
+        return new Result(sum, errors);
     }
+
     private static class Result {
         private double sum;
         private int errors;
+
         public Result(double sum, int errors) {
             this.sum = sum;
-        }
-        public Result(int errors) {
             this.errors = errors;
         }
-        public Result() {}
+
+        public Result() {
+        }
+
         public double errorLines() {
             return errors;
         }
+
         public double getSum() {
             return sum;
         }
+
         public void setSum(double sum) {
             this.sum = sum;
         }
     }
+
     @Test
-    public void testMain (){
+    public void testMain() {
         class TestCase {
             private final String fileName;
             private final Result test;
-            public TestCase(String fileName, Result test) {super();this.fileName = fileName;this.test = test;}
-            public String getFileName() {return fileName;}
-            public Result getTest (){
-                return  test;
-            }}
-        TestCase[] testCases = {new TestCase ("in",new Result(8.24,3)),
-                new TestCase("in2",new Result(30.124,9)),
-                new TestCase("in3",new Result(1.9, 0))};
+
+            public TestCase(String fileName, Result test) {
+                super();
+                this.fileName = fileName;
+                this.test = test;
+            }
+
+            public String getFileName() {
+                return fileName;
+            }
+
+            public Result getTest() {
+                return test;
+            }
+        }
+        TestCase[] testCases = {new TestCase("in", new Result(8.24, 3)),
+                new TestCase("in2", new Result(30.124, 9)),
+                new TestCase("in3", new Result(1.9, 0))};
         for (TestCase test1 : testCases) {
             Result resultTest = getResult(test1.fileName);
-            Assert.assertEquals(resultTest.sum, test1.getTest().sum,0.00001);
+            Assert.assertEquals(resultTest.sum, test1.getTest().sum, 0.00001);
             Assert.assertEquals(resultTest.errors, test1.getTest().errorLines());
-        }}
+        }
+    }
 }
