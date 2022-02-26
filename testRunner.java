a16fb1a25a79131b8dc8538b3aee0ddbdf281f2c
vladimirzhlobin1 <94230029+vladimirzhlobin1@users.noreply.github.com>
2022-02-26 11:28:17 +0300

    add converted files af.rew.Strings2

--- testRunner.java
+++ testRunner.java
@@ -4,17 +4,14 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 public class TestRunner {
     final static String KEY_FIRST = "index(.*)";
     final static String NUM_SEARCH = "[1-9]\\d*";
     final static String VALUE = "value";
     static Pattern pattern = Pattern.compile(KEY_FIRST);
     static Pattern pattern2 = Pattern.compile(NUM_SEARCH);
-
     private static int getResult(String g) throws FileNotFoundException {
         double sum = 0.0;
         int errors = 0;
@@ -42,65 +39,47 @@ public class TestRunner {
                     }
                 }
             }
-            System.out.println("sum = " + sum);
-            System.out.println("error-lines = " + errors);
         } catch (MissingResourceException e) {
             System.out.println("No file...");
         }
-        // it.setSum(sum);
-        return errors;
+        return new Result(sum, errors);;
     }
-
     private static class Result {
-        public double sum;
-        public int errors;
-
-        public Result(double sum) {
+        private double sum;
+        private int errors;
+        public Result(double sum, int errors) {
             this.sum = sum;
         }
-
-        public Result() {
+        public Result(int errors) {
+            this.errors = errors;
         }
-
+        public Result() {}
         public double errorLines() {
             return errors;
         }
-
         public double getSum() {
             return sum;
         }
-
         public void setSum(double sum) {
             this.sum = sum;
         }
     }
-
     @Test
-    public void testMain() {
+    public void testMain (){
         class TestCase {
             private final String fileName;
             private final Result test;
-
-            public TestCase(String fileName, Result test) {
-                super();
-                this.fileName = fileName;
-                this.test = test;
-            }
-
-            public String getFileName() {
-                return fileName;
-            }
-
-            public Result getTest() {
-                return test;
-            }
-        }
-        TestCase[] testCases = {new TestCase("in", new Result(8.24)),
-                new TestCase("in2", new Result(30.124)), new TestCase("in3", new Result(1.9))};
+            public TestCase(String fileName, Result test) {super();this.fileName = fileName;this.test = test;}
+            public String getFileName() {return fileName;}
+            public Result getTest (){
+                return  test;
+            }}
+        TestCase[] testCases = {new TestCase ("in",new Result(8.24,3)),
+                new TestCase("in2",new Result(30.124,9)),
+                new TestCase("in3",new Result(1.9, 0))};
         for (TestCase test1 : testCases) {
             Result resultTest = getResult(test1.fileName);
-            Assert.assertEquals(resultTest.sum, test1.getTest().sum, 0.00001);
+            Assert.assertEquals(resultTest.sum, test1.getTest().sum,0.00001);
             Assert.assertEquals(resultTest.errors, test1.getTest().errorLines());
-        }
-    }
+        }}
 }
